7b10eda3d21cd3ca05a7a681837889c3e02a853e
Arturas Gecas <artgeca@gmail.com>
2022-09-15 14:09:57 +0300

    feat: added type model and validation schemas

--- src/models/type-model.js
+++ src/models/type-model.js
@@ -14,6 +14,25 @@ const typeSchema = new Schema({
   timestamps: true
 });
 
+const typeValidationSchema = yup.object().shape({
+  title: yup
+    .string().typeError('Type.title must be a string')
+    .required('Type.title is required'),
+  img: yup
+    .string().typeError('Type.img must be a string')
+    .required('Type.img is required'),
+});
+
+const typeUpdateValidationSchema = yup.object().shape({
+  title: yup
+    .string().typeError('Type.title must be a string'),
+  img: yup
+    .string().typeError('Type.img must be a string'),
+});
+
+typeSchema.statics.validateType = (typeData) => typeValidationSchema.validate(typeData);
+typeSchema.statics.validateTypeUpdate = (typeData) => typeUpdateValidationSchema.validate(typeData);
+
 const TypeModel = model('Type', typeSchema);
 
 module.exports = TypeModel;
