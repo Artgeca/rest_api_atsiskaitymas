37c727c0da6d45362fee3ac6016fe295df55d880
artgeca <artgeca@gmail.com>
2022-09-24 08:51:51 +0300

    chore: auth controller missing '/'

--- src/routers/auth-router.js
+++ src/routers/auth-router.js
@@ -3,7 +3,7 @@ const { login, register } = require('../controllers/auth-controller');
 
 const authRouter = Router();
 
-authRouter.post('login', login);
-authRouter.post('register', register);
+authRouter.post('/login', login);
+authRouter.post('/register', register);
 
 module.exports = authRouter;
